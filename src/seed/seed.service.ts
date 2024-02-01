8202d538dcecd5c4c398bfc9168bf328704f57ac
Jhonatan Reyes <jbrr.jxr@gmail.com>
2024-01-31 22:49:29 -0600

    add seed module

---
+++ src/seed/seed.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@nestjs/common';
+
+import axios, { AxiosInstance } from 'axios';
+import { PokeResponse } from './interfaces/poke-response.interface';
+import { InjectModel } from '@nestjs/mongoose/dist/common/mongoose.decorators';
+import { Model } from 'mongoose';
+import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
+
+@Injectable()
+export class SeedService {
+  private readonly axios: AxiosInstance = axios;
+
+  constructor(
+    @InjectModel(Pokemon.name) private readonly pokemonModel: Model<Pokemon>,
+  ) {}
+  async excecuteSeed() {
+    const { data } = await this.axios.get<PokeResponse>(
+      'https://pokeapi.co/api/v2/pokemon?limit=650',
+    );
+
+    data.results.forEach(async ({ name, url }) => {
+      // console.log({ name, url });
+
+      const segments = url.split('/');
+      const no = +segments[segments.length - 2];
+
+      const pokemon = await this.pokemonModel.create({ name, no });
+    });
+    return 'Seed executed';
+  }
+}
